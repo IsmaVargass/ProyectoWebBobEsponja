1dd26484be525fe0c7053cfc4e9b7331f15f7bc7
Braulio <brauliopv2h@gmail.com>
2025-05-02 17:16:39 +0200

    Planteamiento header, sticky nav y notificacion js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-"use strict";
+"use strict"
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Programa la función para dentro de 3000 milisegundos (3 s)
+    setTimeout(() => {
+    const respuesta = confirm("¿Quieres estar informado de las novedades?");
+    if (respuesta) {
+        console.log('El usuario respondió OK');
+    } else {
+        console.log('El usuario canceló');
+    }
+    }, 3000);
+});
